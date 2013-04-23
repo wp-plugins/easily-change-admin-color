69b20d738e785ab37e5ae9a2e7b23778b317b455
kfoulks2011 <kfoulks2011@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-23 21:39:42 +0000

    1.0.2 tag
    git-svn-id: https://plugins.svn.wordpress.org/easily-change-admin-color/tags/1.0.2@702508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cam_color_scheme.php
@@ -0,0 +1,14 @@
+<?php
+	echo '
+		<form action="">
+			<select name="cam_schemes" id="schemes">
+				<option selected="selected" value="red">Red</option>
+				<option value="blue">Blue</option>
+				<option value="green">Green</option>
+				<option value="primary">Primary</option>
+				<option value="allow-deny">Allow / Deny</option>
+				<option value="purple">Purple</option>
+			</select>
+			</form>
+		';
+?>
